e9ca5b0d8fea6ca826bffec4753a3d09ae9a4d20
developershuvajit <karmakarshuvajit199@gmail.com>
2026-04-19 18:21:19 +0530

    small changes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>School India Junior - Quality Education for Young Minds</title>
+    <title>School Management Softwere</title>
 
     <!-- Fonts & Icons -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&family=Merriweather:wght@300;400;700&display=swap" rel="stylesheet">
